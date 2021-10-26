3bc7a7296da3ca484c16e999e9ccd29c233939a5
Roberto Milani <milani@strands.com>
2021-10-26 09:30:16 +0200

    app complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-form';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  fuelConsumed: number = 0;
+  distance: number = 0;
+  unit: string = 'km';
+  car = {
+    make: 'Fiat',
+    model: 'Punto',
+    year: 2000
+  }
+
+
+  onNameChange(target: EventTarget | null) {
+    const value = (target as HTMLInputElement).value;
+    this.name = value;
+  }
+
+  onDateChange(target: EventTarget | null) {
+    const value = (target as HTMLInputElement).value;
+    this.date = value;
+  }
+
+  onAmountChange(target: EventTarget | null) {
+    const value = parseFloat((target as HTMLInputElement).value);
+    if (!Number.isNaN(value)) {
+      this.amount = value;
+    } else {
+      this.amount = 0;
+    }
+  }
+
+  onFuelConsumedChange(target: EventTarget | null) {
+    const value = parseFloat((target as HTMLInputElement).value);
+    if (!Number.isNaN(value)) {
+      this.fuelConsumed = value;
+    } else {
+      this.fuelConsumed = 0;
+    }
+  }
+
+  onDistanceChange(target: EventTarget | null) {
+    const value = parseFloat((target as HTMLInputElement).value);
+    if (!Number.isNaN(value)) {
+      this.distance = value;
+    } else {
+      this.distance = 0;
+    }
+  }
+
+  onUnitChange(target: EventTarget | null) {
+    const value = (target as HTMLInputElement).value;
+    this.unit = value;
+  }
 }
